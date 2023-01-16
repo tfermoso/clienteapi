9fd8f1f78cb51df4b921c012259d1246103bf23d
tfermoso <tomas.fermoso@gmail.com>
2023-01-16 19:13:10 +0100

    enviando msg

--- js/index.js
+++ js/index.js
@@ -1,12 +1,19 @@
-document.onload=function(){
+
+window.onload=function(){
+    console.log("cargando");
     $("#btnEnviar").click(()=>{
+        console.log("enviando");
         let id_usuario_origen=$("#idUsuarioOrigen").val();
         let id_usuario_destino=$("#idUsuarioDestino").val();
         let msg=$("#mensaje").val();
-
+        let datos={
+            "id_usuario_origen":id_usuario_origen,
+            "id_usuario_destino":id_usuario_destino,
+            "mensaje":msg
+        }
         $.ajax({
             type:"POST",
-            url:"localhost/mvc/api/new",
+            url:"http://localhost/mvc/api/new",
             data:datos,
             success:(dat)=>{
                 console.log(dat);
