2cbb6c9f545ed25ae26befdbe8b5ae8742141edd
tfermoso <tomas.fermoso@gmail.com>
2023-01-18 17:16:52 +0100

    mostrando mensajes

--- js/index.js
+++ js/index.js
@@ -30,9 +30,22 @@ window.onload=function(){
     })
 
     $.ajax({
-        url:"http://localhost/mvc/api",
+        url:"http://192.168.100.27/mvc/api",
         success:(datos)=>{
-            console.log(datos);
+            let mensajes=JSON.parse(datos);
+            let fila="";
+            for (let index = 0; index < mensajes.length; index++) {
+                const mensaje = mensajes[index];
+                let msg=`<tr>
+                <th scope="row">${mensaje.id}</th>
+                <td>${mensaje.id_usuario_origen}</td>
+                <td>${mensaje.id_usuario_destino}</td>
+                <td>${mensaje.mensaje}</td>
+                <td>${mensaje.fecha_envio}</td>
+              </tr>`;
+              fila+=msg;
+            }
+            $("#tbody").html(fila);
         },
         error:(err)=>{
             console.log(err);
